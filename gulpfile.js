b3b47e4a328186aa540c130dfa5e44314c70b506
wdaxi <2502483420@qq.com>
2018-06-11 21:27:31 +0800

    wdaxi

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-var gulp = require('gulp');
+var gulp = require('gulp'); //引入gulp
 
-var server = require('gulp-webserver');
+var server = require('gulp-webserver'); //引入webserver
 
 
-var less = require('gulp-sass');
+var less = require('gulp-sass'); //编译sass插件
 
-var mincss = require('gulp-clean-css');
+var mincss = require('gulp-clean-css'); //
 var concat = require('gulp-concat');
 
 
@@ -17,9 +17,7 @@ var uglify = require('gulp-uglify');
 gulp.task('css', function() {
     gulp.src('src/**/*.scss')
         .pipe(less())
-        //.pipe(gulp.dest('dist'))
         .pipe(mincss())
-        //.pipe(gulp.dest('dist'))
         .pipe(concat('all.css'))
         .pipe(gulp.dest('dist/css'))
 })
@@ -27,7 +25,6 @@ gulp.task('css', function() {
 gulp.task('js', function() {
     gulp.src('src/**/*.js')
         .pipe(uglify())
-        //.pipe(gulp.dest('dist'))
         .pipe(concat('all.js'))
         .pipe(gulp.dest('dist/js'))
 })
@@ -36,28 +33,27 @@ gulp.task('html', function() {
         .pipe(gulp.dest('dist'))
 })
 
-
-
-
-
 gulp.task('server', function() {
-    gulp.src('dist')
-        .pipe(server({
-            port: 8060,
-            open: true,
-            middleware: function(req, res, next) {
-                var pathname = require('url').parse(req.url, true).pathname;
-                pathname = pathname === '/' ? 'index.html' : pathname;
-                if (req.url === '/favicon.ico') {
-                    return;
-                };
-                res.end(require('fs').readFileSync(require('path').join(__dirname, "dist", pathname)))
-                next();
-                return;
-            }
-        }))
-})
-gulp.task('default', ['server', 'css', 'js', 'html'])
+        gulp.src('dist')
+            .pipe(server({
+                port: 8060,
+                open: true,
+                middleware: function(req, res, next) {
+                    var pathname = require('url').parse(req.url, true).pathname;
+                    pathname = pathname === '/' ? 'index.html' : pathname;
+                    if (req.url === '/favicon.ico') {
+                        return;
+                    };
+                    res.end(require('fs').readFileSync(require('path').join(__dirname, "dist", pathname)))
+                        // next();
+                        // return;
+                }
+            }))
+    })
+    //gulp.task('default', ['server', 'css', 'js', 'html'])
     // gulp.tassk('concat', function() {
     //     gulp.src('src')
     // })
+
+gulp.task('dev', ['server']);
+gulp.task('bulid', ['css', 'js', 'html']);
